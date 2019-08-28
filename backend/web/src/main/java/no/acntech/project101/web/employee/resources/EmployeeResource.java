67c97d8089b9fa5257c379979811f643b05cc268
Piraveen Perinparajan <pira.rajan@gmail.com>
2019-08-28 13:58:41 +0200

    Began work to connect web-layer with db

--- backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
+++ backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
@@ -4,8 +4,13 @@ import java.net.URI;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import no.acntech.project101.employee.Employee;
 import no.acntech.project101.employee.service.EmployeeService;
+import no.acntech.project101.web.employee.resources.converter.EmployeeConverter;
+import no.acntech.project101.web.employee.resources.converter.EmployeeDtoConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,61 +18,105 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("employees")
-//TODO This is a REST controler and should receive request on path employees
 public class EmployeeResource {
 
-    // Global variables
-    final List<EmployeeDto> collect = new ArrayList<EmployeeDto>();
+    // Variables
+    private final EmployeeService employeeService;
+    private final EmployeeDtoConverter employeeDtoConverter;
+    private final EmployeeConverter employeeConverter;
+
 
     //TODO The constructor needs to accept the required dependencies and assign them to class variables
-    public EmployeeResource() {
+    public EmployeeResource(final EmployeeService employeeService,
+                            final EmployeeDtoConverter employeeDtoConverter,
+                            final EmployeeConverter employeeConverter) {
+        this.employeeService = employeeService;
+        this.employeeDtoConverter = employeeDtoConverter;
+        this.employeeConverter = employeeConverter;
     }
 
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> findAll() {
-        EmployeeDto employee1 = new EmployeeDto(1L, "Piraveen", "Perinparajan", LocalDate.of(1995, 9, 20), 78L );
-        EmployeeDto employee2 = new EmployeeDto(2L, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+        final List<Employee> employees = employeeService.findAll();
+        final List<EmployeeDto> collect = employees.stream()
+                .map(employeeDtoConverter::convert)
+                .collect(Collectors.toList());
 
-        collect.add(employee1);
-        collect.add(employee2);
+          // LEGACY
+//        EmployeeDto employee1 = new EmployeeDto(1L, "Piraveen", "Perinparajan", LocalDate.of(1995, 9, 20), 78L );
+//        EmployeeDto employee2 = new EmployeeDto(2L, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+//        final List<EmployeeDto> collect = new ArrayList<EmployeeDto>();
+//        collect.add(employee1);
+//        collect.add(employee2);
         return ResponseEntity.ok(collect);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> findById(@PathVariable final Long id) {
 
-//        for (EmployeeDto employeeDto : collect){
-//            if (employeeDto.getId().equals(id)){
-//                // Found it
-//                return ResponseEntity.ok(employeeDto);
-//            } else {
-//                return ResponseEntity.notFound().build();
-//            }
-//        }
-        EmployeeDto employee2 = new EmployeeDto(id, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+        final Optional<Employee> employee = employeeService.findById(id);
 
+        if (employee.isPresent()){
+            final EmployeeDto convert = employeeDtoConverter.convert(employee.get());
+            return ResponseEntity.ok(convert);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
 
-        return ResponseEntity.ok(employee2);
+        // LEGACY
+//        EmployeeDto employee2 = new EmployeeDto(id, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+//        return ResponseEntity.ok(employee2);
     }
 
     @PostMapping
     public ResponseEntity createEmployee(@RequestBody final EmployeeDto employeeDto) {
+        final Employee convert = employeeConverter.convert(employeeDto);
+        final Employee saved = employeeService.save(convert);
+
         final URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(55L)
+                .buildAndExpand(saved.getId())
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity deleteEmployee(@PathVariable final Long id) {
-        return ResponseEntity.accepted().build();
+        final Optional<Employee> employee = employeeService.findById(id);
+
+        if (employee.isPresent()) {
+            employeeService.delete(id);
+            return ResponseEntity.accepted().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        // LEGACY
+//        return ResponseEntity.accepted().build();
     }
 
     @PatchMapping("{id}")
     public ResponseEntity updateEmployee(@PathVariable final Long id, @RequestBody final EmployeeDto employeeDto) {
-        EmployeeDto employee = new EmployeeDto(id, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+        final Optional<Employee> optionalEmployee = employeeService.findById(id);
 
-        return ResponseEntity.ok(employee);
+        if(optionalEmployee.isPresent()){
+            Employee existingEmployee = optionalEmployee.get();
+            existingEmployee.setFirstName(employeeDto.getFirstName());
+            existingEmployee.setLastName(employeeDto.getLastName());
+            existingEmployee.setDateOfBirth(employeeDto.getDateOfBirth());
+            existingEmployee.setCompanyId(employeeDto.getCompanyId());
+
+            Employee saved = employeeService.save(existingEmployee);
+
+            final EmployeeDto convert = employeeDtoConverter.convert(saved);
+            return ResponseEntity.ok(convert);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+        // LEGACY
+//        EmployeeDto employee = new EmployeeDto(id, "Ola", "Nordmann", LocalDate.of(1950, 4, 20), 78L );
+//
+//        return ResponseEntity.ok(employee);
     }
 }
--- backend/web/src/main/java/no/acntech/project101/web/employee/resources/converter/EmployeeDtoConverter.java
+++ backend/web/src/main/java/no/acntech/project101/web/employee/resources/converter/EmployeeDtoConverter.java
@@ -11,7 +11,10 @@ public class EmployeeDtoConverter implements Converter<Employee, EmployeeDto> {
 
     @Override
     public EmployeeDto convert(final Employee source) {
-        //TODO map from employee to dto
-        return null;
+        return new EmployeeDto(source.getId(), source.getFirstName(), source.getLastName(), source.getDateOfBirth(), source.getCompanyId());
     }
 }
+
+
+// EmployeeDto er det som serveres ut til brukeren (slik kan man filtrere hvilke felt som faktisk vises
+// til brukeren. På den måten trenger man ikke vise _alle_ felt i Employee-klassen.
