14f2f28aade59c8761e81a7da1f7876bcc56ff09
Piraveen Perinparajan <pira.rajan@gmail.com>
2019-08-28 13:02:48 +0200

    Implement domain- and service-layer

--- backend/domain/src/main/java/no/acntech/project101/employee/Employee.java
+++ backend/domain/src/main/java/no/acntech/project101/employee/Employee.java
@@ -1,11 +1,73 @@
 package no.acntech.project101.employee;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.time.LocalDate;
+
+
+@Entity
 public class Employee {
 
-    //TODO Create the enitity for Employee
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "DATE_OF_BIRTH")
+    private LocalDate dateOfBirth;
+
+//    @Column(name = "COMPANY_ID")
+//    private long companyId;
 
     public Employee() {
-        // Hibernate
+    }
+
+    public Employee(final String firstName, final String lastName, final LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+//        this.companyId = companyId;
+    }
+
+    //  Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 
 }
--- backend/service/src/main/java/no/acntech/project101/employee/config/EmployeeDatabaseConfig.java
+++ backend/service/src/main/java/no/acntech/project101/employee/config/EmployeeDatabaseConfig.java
@@ -1,6 +1,16 @@
 package no.acntech.project101.employee.config;
 
 //TODO add configuration for repository to enable the jpa repository created
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "no.acntech.project101.employee.repository")
+@EntityScan(basePackageClasses = {Jsr310JpaConverters.class},
+        basePackages = "no.acntech.project101.employee")
 public class EmployeeDatabaseConfig {
 
 }
